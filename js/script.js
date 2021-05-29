632dd54139de10978aad263c0c25fedb719df5a8
esmit000 <80164186+esmit000@users.noreply.github.com>
2021-05-29 15:10:38 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let menuToggle = document.querySelector(".menu-toggle");
+
+let menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", (e) => {
+  menu.classList.toggle("mostrar");
+});
